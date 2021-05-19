dccd615fa52d88b9055b4d74eecd081e8bbfb962
kevinkobori <kevin.kobori@hotmail.com>
2021-05-19 14:43:25 -0300

    ...

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -13,105 +13,46 @@ class ProductCard extends StatefulWidget {
     this.onTapPrevious,
     this.width,
     this.isEnabled,
-  }); //this.currentIndex,
+  });
   final List<ProductModel> productsList;
   final int index;
   final int currentIndex;
-  bool isEnabled;
-  VoidCallback onTapPrevious;
-  VoidCallback onTapNext;
+  final bool isEnabled;
+  final VoidCallback onTapPrevious;
+  final VoidCallback onTapNext;
   final double width;
   @override
   _ProductCardState createState() => _ProductCardState();
 }
 
 class _ProductCardState extends State<ProductCard> {
-  Image image1;
-  Image image2;
-  Image image3;
-  Image image4;
-  Image image5;
-  Image image6;
-  Image image7;
-  Image image8;
-  Image image9;
-  Image image10;
-  Image image11;
-  Image image12;
-  Image image13;
-  Image image14;
-  Image image15;
-  Image imagebg;
-  Image imagebg2;
-  // bool widget.isEnabled = true;
   bool isHover = false;
   bool isHoverPrevious = false;
   bool isHoverNext = false;
-
+  List<Image> images = [];
+  ProductModel product;
+  Image imagebg;
+  Image imagebg2;
+  Image image;
   @override
   void initState() {
     super.initState();
-    // Future.delayed(Duration(milliseconds: 1201)).then((value) {
-    //   setState(() {
-    //     widget.isEnabled = true;
-    //   });
-    // });
-    image1 = Image.asset(widget.productsList[0].image);
-    image2 = Image.asset(widget.productsList[1].image);
-    image3 = Image.asset(widget.productsList[2].image);
-    image4 = Image.asset(widget.productsList[3].image);
-    image5 = Image.asset(widget.productsList[4].image);
-    image6 = Image.asset(widget.productsList[0].imageBlur);
-    image7 = Image.asset(widget.productsList[1].imageBlur);
-    image8 = Image.asset(widget.productsList[2].imageBlur);
-    image9 = Image.asset(widget.productsList[3].imageBlur);
-    image10 = Image.asset(widget.productsList[4].imageBlur);
-    image11 = Image.asset(widget.productsList[0].imageBlur);
-    image12 = Image.asset(widget.productsList[1].imageBlur2);
-    image13 = Image.asset(widget.productsList[2].imageBlur2);
-    image14 = Image.asset(widget.productsList[3].imageBlur2);
-    image15 = Image.asset(widget.productsList[4].imageBlur2);
-    imagebg = Image.asset('assets/images/bg.png');
-    imagebg2 = Image.asset('assets/images/bg_2.png');
+    for (product in widget.productsList) {
+      images.add(Image.asset(product.image));
+      images.add(Image.asset(product.imageBlur));
+      images.add(Image.asset(product.imageBlur2));
+    }
+    images.add(Image.asset('assets/images/bg.png'));
+    images.add(Image.asset('assets/images/bg_2.png'));
   }
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    // Future.delayed(Duration(milliseconds: 1201)).then((value) {
-    //   setState(() {
-    //     widget.isEnabled = true;
-    //   });
-    // });
-    precacheImage(image1.image, context);
-    precacheImage(image2.image, context);
-    precacheImage(image3.image, context);
-    precacheImage(image4.image, context);
-    precacheImage(image5.image, context);
-    precacheImage(image6.image, context);
-    precacheImage(image7.image, context);
-    precacheImage(image8.image, context);
-    precacheImage(image9.image, context);
-    precacheImage(image10.image, context);
-    precacheImage(image11.image, context);
-    precacheImage(image12.image, context);
-    precacheImage(image13.image, context);
-    precacheImage(image14.image, context);
-    precacheImage(image15.image, context);
-    precacheImage(imagebg.image, context);
-    precacheImage(imagebg2.image, context);
+    for (image in images) {
+      precacheImage(image.image, context);
+    }
   }
-
-  // waitToPressAgain() {
-  //   setState(() {
-  //     widget.isEnabled = false;
-  //   });
-  //   Future.delayed(Duration(milliseconds: 1201)).then((value) {
-  //     setState(() {
-  //       widget.isEnabled = true;
-  //     });
-  //   });
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -120,13 +61,13 @@ class _ProductCardState extends State<ProductCard> {
         : widget.currentIndex - 1;
     return Stack(
       children: [
-        AnimatedContainer(
-          duration: Duration(milliseconds: 0),
+        SizedBox(
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.height,
           child: widget.currentIndex == widget.index ||
                   widget.currentIndex == widget.index - 1 ||
-                  widget.currentIndex - widget.productsList.length == widget.index - 1
+                  widget.currentIndex - widget.productsList.length ==
+                      widget.index - 1
               ? Hero(
                   tag: widget.productsList[widget.index].id,
                   child: Image.asset(
@@ -147,6 +88,51 @@ class _ProductCardState extends State<ProductCard> {
                       ),
                     ),
         ),
+        // Positioned.fill(
+        //   child: Hero(
+        //     tag: widget.productsList[widget.index].id,
+        //     child: Image.asset(
+        //       widget.productsList[widget.index].image,
+        //     ),
+        //   ),
+        // ),
+        // AnimatedContainer(
+        //   duration: Duration(milliseconds: 0),
+        //   height: MediaQuery.of(context).size.height,
+        //   width: MediaQuery.of(context).size.height,
+        //   child: widget.currentIndex == widget.index ||
+        //           widget.currentIndex == widget.index - 1 ||
+        //           widget.currentIndex - widget.productsList.length ==
+        //               widget.index - 1
+        //       ? BackdropFilter(
+        //           filter: ImageFilter.blur(
+        //             sigmaX: 1,
+        //             sigmaY: 1,
+        //           ),
+        //           child: Container(
+        //             color: Colors.transparent,
+        //           ),
+        //         )
+        //       : i1 == widget.index
+        //           ? BackdropFilter(
+        //               filter: ImageFilter.blur(
+        //                 sigmaX: 100,
+        //                 sigmaY: 100,
+        //               ),
+        //               child: Container(
+        //                 color: Colors.transparent,
+        //               ),
+        //             )
+        //           : BackdropFilter(
+        //               filter: ImageFilter.blur(
+        //                 sigmaX: 100,
+        //                 sigmaY: 100,
+        //               ),
+        //               child: Container(
+        //                 color: Colors.transparent,
+        //               ),
+        //             ),
+        // ),
         Container(
           width: MediaQuery.of(context).size.height,
           height: MediaQuery.of(context).size.height,
@@ -167,7 +153,6 @@ class _ProductCardState extends State<ProductCard> {
                             isHoverPrevious = false;
                           });
                           widget.onTapPrevious();
-                          // waitToPressAgain();
                         }
                       : () {},
                   child: isHoverPrevious
